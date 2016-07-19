2aaa7ce354240fd1a5d9bc1aadcf84e7fda48c0c
Jake Mitchell <legomanaz@gmail.com>
2016-07-19 11:57:05 -0700

    eygdsll;';oj

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,14 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
-		
-		
+Robot rawsteal = new Robot ();
+rawsteal.setPenColor(0,204,221);
+	rawsteal.penDown();
+	for (int i = 0; i <5; i++) {
+		rawsteal.move(100);
+		rawsteal.turn(140);}
 	}
-}
+	
+	}
+	
+
--- src/day1/robot/PentagonCrazy.java
+++ src/day1/robot/PentagonCrazy.java
@@ -10,27 +10,33 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-
+Robot rambie = new Robot ();
 		// 3. Put the robot's pen down
-
+rambie.penDown();
 		// 8. Make the robot go at maximum speed (10)
-
+rambie.setSpeed(10);
 		// 9. Set the pen to a color that you like for the shape
-
+rambie.setRandomPenColor();
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
+int sides = 5;
 		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
-
+int angle = 360/5;
 		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
-
-			// 2. Move the robot 200 pixels
+for (int i = 0; i < 200; i++) {
 	
+	rambie.setRandomPenColor();
+			// 2. Move the robot 200 pixels
+	rambie.move(i);
 			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
 	
 			// 6. Turn the robot the amount in your angle variable
+
+		rambie.turn(360/5);
+		
 	
 			// 11. Turn the robot one more degree
-
+		rambie.turn(1);
+}
 	}
 
 	// Variations:
